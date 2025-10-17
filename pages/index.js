57351b1803fe054c041f5cf8ee75e05d1266b562
Rafael Andrade <oi@rafaelandrade.dev>
2025-10-17 17:01:31 -0300

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-  return <h1>Celso portioli não teve nada a ver com o 12 de setembro.</h1>
+function Home() {
+  return <h1>Celso portioli não teve nada a ver com o 12 de setembro.</h1>;
 }
 
 export default Home;
